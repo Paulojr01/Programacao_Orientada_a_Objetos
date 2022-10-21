bb4f0c0d4e1853562d4ede5f0413592168afd939
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2022-10-20 22:20:09 -0300

    Descrição_Aula
    Explicações feita dentro de sala usando for com funções lambda

--- aula0809__/Main.java
+++ aula0809__/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner ler = new Scanner(System.in);
 
             int n = ler.nextInt();
